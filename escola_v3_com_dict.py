4d3312fcd68e0eccfe854d09253807e583f857cc
Daniel Volponi <volponidaniel@gmail.com>
2022-04-09 13:53:05 -0300

    Atualizado o script com o uso de dicionaris nas variaveis salas e aulas extras

--- escola_v3_com_dict.py
+++ escola_v3_com_dict.py
@@ -4,21 +4,26 @@
 Imprimir a lista de crianças agrupadas por sala
 que frequentam cada uma das atividades.
 """
-__version__ = "0.1.1"
+__version__ = "0.1.2"
 
 # Dados
-sala1 = ["Erik", "Maia", "Gustavo", "Manuel", "Sofia", "Joana"]
-sala2 = ["Joao", "Antonio", "Carlos", "Maria", "Isolda"]
+salas = {
+    "sala1": ["Erik", "Maia", "Gustavo", "Manuel", "Sofia", "Joana"],
+    "sala2": ["Joao", "Antonio", "Carlos", "Maria", "Isolda"]        
+}
 
 # Salas de atividades extra
-aula_ingles = ["Erik", "Maia", "Joana", "Carlos", "Antonio"]
-aula_musica = ["Erik", "Carlos", "Maria"]
-aula_danca = ["Gustavo", "Sofia", "Joana", "Antonio"]
+
+aulas_extras = {
+    "ingles": ["Erik", "Maia", "Joana", "Carlos", "Antonio"],
+    "musica": ["Erik", "Carlos", "Maria"],
+    "danca": ["Gustavo", "Sofia", "Joana", "Antonio"] 
+}
 
 atividades = {
-    "Inglês": aula_ingles,
-    "Música": aula_musica,
-    "Dança": aula_danca
+    "Inglês": aulas_extras["ingles"],
+    "Música": aulas_extras["musica"],
+    "Dança": aulas_extras["danca"]
 }
 
 # Listar alunos em cada atividade por sala
@@ -29,8 +34,8 @@ for nome_atividade, atividade in atividades.items():
     print("-" * 50)
     
     # sala1 que tem a interseção com a atividade
-    atividade_sala1 = set(sala1) & set(atividade)
-    atividade_sala2 = set(sala2) & set(atividade)
+    atividade_sala1 = set(salas["sala1"]) & set(atividade)
+    atividade_sala2 = set(salas["sala2"]) & set(atividade)
     
     print("Sala1 ", atividade_sala1)
     print("Sala2 ", atividade_sala2)
