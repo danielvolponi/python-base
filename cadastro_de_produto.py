8cb649348e0611221b04bdb978a5f71eca041329
Daniel Volponi <volponidaniel@gmail.com>
2022-04-09 13:30:14 -0300

    Criado o script escola_v3_com_dict

--- cadastro_de_produto.py
+++ cadastro_de_produto.py
@@ -2,22 +2,40 @@
 """Cadastro de Produto"""
 __version_ = "0.1.0"
 
-produto_nome = "Caneta"
-produto_cor1 = "azul"
-produto_cor2 = "branco"
-produto_preco = 3.23
-produto_dimensao_altura = 12.1
-produto_dimensao_largura = 0.8
-produto_em_estoque = True
-produto_codigo = 45678
-produto_codebar = None
+import pprint
 
+
+produto = {
+    "nome": "Caneta",
+    "cores":["azul", "branco"],
+    "preco": 3.23,
+    "dimensao": {
+        "altura": 12.1,
+        "largura": 0.8,  
+    },
+    "em_estoque": True,
+    "codigo": 45678,
+    "codebar": None
+}
 
 
 # Compra por um cliente
-compra = ("Bruno", produto_nome, 3)
-total_compra = compra[2] * produto_preco
+cliente = {
+    "nome": "Bruno"
+}
+
+compra = {
+    "cliente": cliente,
+    "produto": produto,
+    "quantidade": 3
+}
+
+# pprint.pprint(compra)
+
+total_compra = compra['quantidade'] * compra['produto']["preco"]
 
 print(
-    f"O cliente {compra[0]} comprou {compra[1]}"
-    f" e pagou o total de {total_compra}")
+    f"O cliente {compra['cliente']['nome']}"
+    f" comprou {compra['quantidade']} unidades de {compra['produto']['nome']}"
+    f" e pagou o total de {total_compra}"
+)
