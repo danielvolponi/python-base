e45b67599b4f4044c80c75152b2a4842589ce936
Daniel Vaz Volponi <daniel.volponi@nb-will0279.avista.local>
2022-04-20 08:58:49 -0300

    Atualizado o script hello.py com o uso de CLI args e inputs

--- hello.py
+++ hello.py
@@ -10,22 +10,46 @@ Tenha a variável LANG devidamente configura ex:
 
     export LANG=pt_BR
 
+Ou informe atraves do CLI argument '--lang'
+
+Ou usuário terá que digitar
+
 Execução:
 
     python3 hello.py
     ou
     ./hello.py
 """
-__version__ = "0.1.2"
+__version__ = "0.1.3"
 __author__ = "Daniel Volponi"
 __license__ = "Unlicense"
 
 import os
+import sys
 
-current_language = os.getenv("LANG", "en_US")[:5]
+arguments = {"lang": None, "count": 1}
 
-# sets (Hash Table) - o(1) - constante
-# dicionarios (Hash Table) - tempo de look up constante
+for arg in sys.argv[1:]:
+    # TODO: Tratar ValueError
+    key, value = arg.split("=")
+    key = key.lstrip("-").strip()
+    value = value.strip()
+    if key not in arguments:
+        print(f"Invalid Option `{key}`")
+        sys.exit()
+    arguments[key] = value
+    
+current_language = arguments["lang"] 
+
+if current_language is None:
+    # TODO: Usar repetição
+    if "LANG" in os.environ:
+        current_language = os.getenv("LANG")
+    else:
+        current_language = input("Choose a language:")
+
+current_language = current_language[:5]
+
 msg = {
     "en_US": "Hello, World!",
     "pt_BR": "Olá, Mundo!",
@@ -34,15 +58,5 @@ msg = {
     "fr_FR": "Bonjour, Monde!",
 }
 
-
-# Ordem de Complexidade o(n)
-# if current_language == "pt_BR":
-    # msg = "Olá, Mundo!"
-# elif current_language == "it_IT":
-    # msg = "Ciao, Mondo!"
-# elif current_language == "es_SP":
-    # msg = "Hola, Mundo!"
-# elif current_language == "fr_FR":
-    # msg = "Bonjour, Monde!"
         
-print(msg[current_language])
+print(msg[current_language] * int(arguments["count"]))
