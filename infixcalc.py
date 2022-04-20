691a56ab2300caa8d94f14d89a27aa0c83cb2907
Daniel Vaz Volponi <daniel.volponi@nb-will0279.avista.local>
2022-04-20 09:09:44 -0300

    Estrutura desafio infixcalc.py

---
+++ infixcalc.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+"""Calculadora infix.
+
+Funcionamento: 
+
+[operacao] [n1] [n2]
+
+Operações:
+sum -> +
+sub -> -
+mul -> *
+div -> /
+
+Uso:
+$ infixcalc.py sum 5 2
+7
+
+$ infixcalc.py mul 10 5
+50
+
+$ infixcalc.py 
+operação: sum
+n1: 5
+n2: 4
+9
+"""
+__version__ = "0.1.0"
+__author__ = "Daniel Volponi"
+__license__ = "Unlicense"
