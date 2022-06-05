eaf8895b09b5ed4a7789135d74c233b3924a1fdb
Daniel Volponi <volponidaniel@gmail.com>
2022-06-05 16:29:19 -0300

    Erro: EAFP, try, except

--- errors.py
+++ errors.py
@@ -7,15 +7,29 @@ Programa que imprime na tela a posicao 1 da lista de nomes
 import os
 import sys
 
-if os.path.exists("names.txt"):
-    names  = open("names.txt").readlines()
-else:
-    print("[Error] File names.txt not found.")
-    sys.exit(1)
+# EAFP - Easy to Ask Forgiveness than permission
+# (É mais fácil pedir perdão do que permissão)
 
-# LBYL - Look Before you leap
-if len(names) >=3:
+try:
+    raise RuntimeError("Ocorreu um erro")
+except Exception as e:
+    print(str(e)) 
+
+
+try:
+    names  = open("names.txt").readlines()
+except FileNotFoundError as e:
+    print(f"{str(e)}")
+    sys.exit(1)
+    # TODO: Usar retry
+else: 
+    print("Sucesso") # Somente se o try funcionar
+finally:
+    print("Execute isso sempre!") 
+
+
+try:
     print(names[2])
-else:
+except:
     print("[Error] Missing name in the list")
     sys.exit(1)
