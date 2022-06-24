136e2b36efe244e88ee008cb1483b79d1b9c3494
Daniel Vaz Volponi <daniel.volponi@nb-will0279.avista.local>
2022-06-24 08:53:29 -0300

    Modificado o arquivo de introdução a funções

--- funcoes.py
+++ funcoes.py
@@ -36,8 +36,38 @@ def heron(a, b, c):
     # Calcula a area de um triangulo
     perimeter = a + b + c
     s = perimeter / 2
-    area = (s * (s - a) * (s - b) * (s - c)) ** 0.5
-    return area
+    area = (s * (s - a) * (s - b) * (s - c)) 
+    return area ** 0.5
 
-area_triangulo = heron(3, 4, 5)
-print(area_triangulo)
+def heron2(params):
+    return heron(*params)
+
+
+
+triangulos = [
+    (3, 4, 5),
+    (5, 12, 13),
+    (8, 5, 17),
+    (12, 35, 37),
+    (3, 4, 5),
+    (5, 12, 13),
+    (8, 5, 17),
+    (12, 35, 37)
+]
+
+# print(list((map(heron2, triangulos))))
+
+
+for t in triangulos:
+     area = heron2(t)
+     print(f"A área do triangulo é: {area}")
+
+
+###
+
+def nome_da_funcao():
+    print("Hello funcao")
+    return 1
+
+result = nome_da_funcao()
+print(result)
