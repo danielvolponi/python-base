e53822459e030812896f5492600e16ca29a45b2c
Daniel Vaz Volponi <daniel.volponi@nb-will0279.avista.local>
2022-07-11 19:28:55 -0300

    Usando o retry com for

--- errors.py
+++ errors.py
@@ -6,30 +6,27 @@ Programa que imprime na tela a posicao 1 da lista de nomes
 """
 import os
 import sys
+import time
+import logging
+
+log = logging.Logger("errors")
 
 # EAFP - Easy to Ask Forgiveness than permission
 # (É mais fácil pedir perdão do que permissão)
 
-try:
-    raise RuntimeError("Ocorreu um erro")
-except Exception as e:
-    print(str(e)) 
+def try_to_open_a_file(filepath, retry=1) -> list: 
+    """Tries to open a file, if error, retries n times """
+    for attempt in range(1, retry + 1):
+        try:
+            return  open(filepath).readlines()
+        except FileNotFoundError as e:
+            log.error("ERRO: %s", e) 
+            time.sleep(1)
+        else: 
+            print("Sucesso") # Somente se o try funcionar
+        finally:
+            print("Execute isso sempre!")
+    return []     
 
-
-try:
-    names  = open("names.txt").readlines()
-except FileNotFoundError as e:
-    print(f"{str(e)}")
-    sys.exit(1)
-    # TODO: Usar retry
-else: 
-    print("Sucesso") # Somente se o try funcionar
-finally:
-    print("Execute isso sempre!") 
-
-
-try:
-    print(names[2])
-except:
-    print("[Error] Missing name in the list")
-    sys.exit(1)
+for line in try_to_open_a_file("names.txt", retry=5):
+    print(line)
